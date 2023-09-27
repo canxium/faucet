a1551985a2937795e4f718d3624bd2afacae6392
Neo Krypt <founder@canxium.org>
2023-09-27 10:31:03 +0700

    fix next build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,14 @@ import type { AppProps } from "next/app"; // Types
 import { SessionProvider } from "next-auth/react"
 import "react-toastify/dist/ReactToastify.css"; // Toast styles
 import { ToastContainer } from "react-toastify"; // Toast notifications
+import { Session } from "next-auth";
 
-export default function MultiFaucet({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     // Wrap app in auth session provider
     <SessionProvider session={pageProps.session}>
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -5,17 +5,17 @@ export const authOptions = {
   providers: [
     // Twitter OAuth provider
     TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET,
+      clientId: process.env.TWITTER_CLIENT_ID || "",
+      clientSecret: process.env.TWITTER_CLIENT_SECRET || "",
       version: "2.0", // opt-in to Twitter OAuth 2.0
     }),
   ],
   secret: process.env.NEXTAUTH_JWT_SECRET,
   callbacks: {
-    async redirect({ url, baseUrl }) {
+    async redirect({ baseUrl }: any) {
       return baseUrl
     },
-    async session({ session, user, token }) {
+    async session({ session, user, token }: any) {
       session.twitter_id = token.twitter_id;
       session.twitter_handle = token.twitter_handle;
       session.twitter_num_tweets = token.twitter_num_tweets;
@@ -24,7 +24,7 @@ export const authOptions = {
 
       return session
     },
-    async jwt({ token, user, account, profile }) {
+    async jwt({ token, user }: any) {
        // Check if user is signing in (versus logging out)
       const isSignIn = user ? true : false;
 
@@ -40,7 +40,7 @@ export const authOptions = {
       
       return token
     },
-    async signIn({ user, account, profile, email, credentials }) {
+    async signIn({ }) {
       return true
     },
   }
